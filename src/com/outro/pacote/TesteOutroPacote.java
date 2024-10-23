2c8693a76cd7550aa55f2008ea0f17e689ed50e5
unknown <joaopburgos@edu.unifil.br>
2024-10-22 21:52:37 -0300

    first commit

---
+++ src/com/outro/pacote/TesteOutroPacote.java
@@ -0,0 +1,8 @@
+package com.outro.pacote;
+
+public class TesteOutroPacote {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.setNome("Bobby");
+        animal.fazerSom();
+}
